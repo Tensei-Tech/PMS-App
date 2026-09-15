574a794e8e019664b18fb6c48c752f203e4111e6
Apurva Bhoyar <apurvabhoyar005@gmail.com>
2026-09-15 18:37:51 +0530

    style: apply dart format to voice_web_helper.dart

--- frontend/lib/widgets/common_form/voice_web_helper.dart
+++ frontend/lib/widgets/common_form/voice_web_helper.dart
@@ -1,3 +1,2 @@
 // lib/widgets/common_form/voice_web_helper.dart
-export 'voice_web_stub.dart'
-    if (dart.library.js_interop) 'voice_web_impl.dart';
+export 'voice_web_stub.dart' if (dart.library.js_interop) 'voice_web_impl.dart';
